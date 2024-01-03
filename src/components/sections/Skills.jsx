df4b727c73b953372d7dc50b85ea883f92739f07
Gustavo Choque <gustavochoque0791@gmail.com>
2024-01-03 14:01:15 -0300

    skills section updated

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import ReactLogo from "../../assets/react.svg?react";
 
 function Skills() {
   return (
-    <div id="skills" className="min-h-screen">Skills</div>
-  )
+    <section id="skills" className="min-h-screen min-h-dvh flex flex-col justify-center items-center p-16 px-28">
+      <div className="flex gap-6 pb-10 justify-center">
+        <ReactLogo className="" style={{ width: "50px", height: "50px" }} />
+        <h2 className="text-2xl md:text-5xl font-medium">My Expertise</h2>
+      </div>
+      <div className="flex gap-10 justify-center px-10">
+        <article className="p-12 border-2 rounded-2xl max-w-screen-sm">
+          <h3 className="text-xl md:text-2xl font-medium">
+            Front-End Developer
+          </h3>
+          <p className="text-sm md:text-xl font-normal">
+            Experienced in JavaScript, HTML, CSS and React framework.
+          </p>
+          <p className="text-sm md:text-xl font-normal">
+            Knowledge of TailwindCSS, Material UI and most popular technologies.
+          </p>
+        </article>
+        <article className="p-12 border-2 rounded-2xl max-w-screen-sm">
+          <h3 className="text-xl md:text-2xl font-medium">
+            Front-End Developer
+          </h3>
+          <p className="text-sm md:text-xl font-normal">
+            Experienced in JavaScript, HTML, CSS and React framework.
+          </p>
+          <p className="text-sm md:text-xl font-normal">
+            Knowledge of TailwindCSS, Material UI and most popular technologies.
+          </p>
+        </article>
+      </div>
+    </section>
+  );
 }
 
-export default Skills
+export default Skills;
