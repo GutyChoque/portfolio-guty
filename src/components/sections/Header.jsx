8d0c60aa67edd7494137d6f2a105316f19daa72f
Gustavo Choque <gustavochoque0791@gmail.com>
2024-01-02 21:44:27 -0300

    changed header style beheavor

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -2,8 +2,10 @@ import { useState, useEffect } from "react";
 import Nav from "./Nav";
 function Header() {
   const [isSticky, setSticky] = useState(false);
+  const [hasScrolled, setHasScrolled] = useState(false);
 
   const checkScrollTop = () => {
+    setHasScrolled(window.scrollY > 80);
     setSticky(window.scrollY > window.innerHeight);
   };
 
@@ -13,29 +15,24 @@ function Header() {
   }, []);
 
   return (
-    <>
-      <header className="left-0 right-0 z-50 grid grid-cols-3 items-center p-6">
-        <h2
-          className={`${
-            isSticky ? "hidden" : ""
-          } ml-4 text-2xl text- font-bold col-start-1`}
-        >
-          Algun Texto?
-        </h2>
-        <Nav />
-        <div className="col-start-3 col-span-1"></div>
-      </header>
-      <header
+    <header
+      className={`${
+        isSticky
+          ? "fixed top-0 transition-transform duration-700 transform translate-y-0 backdrop-blur-sm"
+          : hasScrolled
+          ? "transform -translate-y-full"
+          : ""
+      } left-0 right-0 z-50 grid grid-cols-3 items-center p-6`}
+    >
+      <h2
         className={`${
-          isSticky
-            ? "fixed top-0 transition-opacity duration-700 opacity-100 backdrop-blur-sm"
-            : "opacity-0"
-        } left-0 right-0 z-50 grid grid-cols-3 items-center p-6`}
+          isSticky ? "hidden" : ""
+        } ml-4 text-2xl text- font-bold col-start-1`}
       >
-        <Nav />
-        <div className="col-start-3 col-span-1"></div>
-      </header>
-    </>
+        Algun Texto?
+      </h2>
+      <Nav />
+    </header>
   );
 }
 
