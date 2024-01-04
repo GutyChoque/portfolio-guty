ffd98010f40c7a23345e1ba370eccaf64a36c2e3
Gustavo Choque <gustavochoque0791@gmail.com>
2024-01-04 20:34:35 -0300

    styles added

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import Nav from "./Nav";
+import gutyFace from "../../assets/images/guty-face.jpeg";
 function Header() {
   const [isSticky, setSticky] = useState(false);
   const [hasScrolled, setHasScrolled] = useState(false);
 
   const checkScrollTop = () => {
     setHasScrolled(window.scrollY > 30);
-    setSticky(window.scrollY > window.innerHeight);
+    setSticky(window.scrollY * 2 > window.innerHeight);
   };
 
   useEffect(() => {
@@ -24,13 +25,14 @@ function Header() {
           : ""
       } left-0 right-0 z-50 grid grid-cols-1 md:grid-cols-3 items-center p-6`}
     >
-      <h2
+      <div
         className={`${
-          isSticky ? "hidden" : "lg:block"
-        } hidden ml-4 text-2xl text- font-bold col-start-1`}
+          isSticky ? "hidden" : "lg:flex"
+        } hidden items-center col-start-1`}
       >
-        Algun Texto?
-      </h2>
+        <img src={gutyFace} alt="Guty" className="rounded-round w-16" />
+        <h2 className="pl-4 text-2xl text- font-bold">Algun Texto?</h2>
+      </div>
       <Nav />
     </header>
   );
--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -2,13 +2,13 @@ import ReactLogo from "../../assets/icons/react.svg?react";
 
 function Skills() {
   return (
-    <section id="skills" className="min-h-screen min-h-dvh flex flex-col justify-center items-center p-4 md:p-16 lg:px-28">
+    <section id="skills" className="min-h-screen min-h-dvh flex flex-col justify-center items-center md:p-16 lg:px-28">
       <div className="flex gap-2 md:gap-6 pb-5 md:pb-10 justify-center">
         <ReactLogo className="" style={{ width: "50px", height: "50px" }} />
         <h2 className="text-xl md:text-2xl lg:text-5xl font-medium">My Expertise</h2>
       </div>
-      <div className="flex flex-col md:flex-row gap-5 md:gap-10 justify-center px-2 md:px-10">
-        <article className="p-6 md:p-12 border-2 rounded-2xl w-[500px]">
+      <div className="flex flex-col md:flex-row gap-5 md:gap-10 justify-center items-center md:px-10">
+        <article className="p-6 md:p-12 border-2 rounded-2xl sm:text-center w-3/4">
           <h3 className="text-lg md:text-xl lg:text-2xl font-medium">
             Front-End Developer
           </h3>
@@ -19,7 +19,7 @@ function Skills() {
             Knowledge of TailwindCSS, Material UI and most popular technologies.
           </p>
         </article>
-        <article className="p-6 md:p-12 border-2 rounded-2xl max-w-screen-sm md:w-[500px]">
+        <article className="p-6 md:p-12 border-2 rounded-2xl w-3/4">
           <h3 className="text-lg md:text-xl lg:text-2xl font-medium">
             Front-End Developer
           </h3>
