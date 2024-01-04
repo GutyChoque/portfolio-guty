a087a89c6464d8ce459c215860300e76dbddd119
Gustavo Choque <gustavochoque0791@gmail.com>
2024-01-04 15:37:31 -0300

    responsive designed

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -2,31 +2,31 @@ import ReactLogo from "../../assets/react.svg?react";
 
 function Skills() {
   return (
-    <section id="skills" className="min-h-screen min-h-dvh flex flex-col justify-center items-center p-16 px-28">
-      <div className="flex gap-6 pb-10 justify-center">
+    <section id="skills" className="min-h-screen min-h-dvh flex flex-col justify-center items-center p-4 md:p-16 lg:px-28">
+      <div className="flex gap-2 md:gap-6 pb-5 md:pb-10 justify-center">
         <ReactLogo className="" style={{ width: "50px", height: "50px" }} />
-        <h2 className="text-2xl md:text-5xl font-medium">My Expertise</h2>
+        <h2 className="text-xl md:text-2xl lg:text-5xl font-medium">My Expertise</h2>
       </div>
-      <div className="flex gap-10 justify-center px-10">
-        <article className="p-12 border-2 rounded-2xl max-w-screen-sm">
-          <h3 className="text-xl md:text-2xl font-medium">
+      <div className="flex flex-col md:flex-row gap-5 md:gap-10 justify-center px-2 md:px-10">
+        <article className="p-6 md:p-12 border-2 rounded-2xl w-[500px]">
+          <h3 className="text-lg md:text-xl lg:text-2xl font-medium">
             Front-End Developer
           </h3>
-          <p className="text-sm md:text-xl font-normal">
+          <p className="text-xs md:text-sm lg:text-xl font-normal">
             Experienced in JavaScript, HTML, CSS and React framework.
           </p>
-          <p className="text-sm md:text-xl font-normal">
+          <p className="text-xs md:text-sm lg:text-xl font-normal">
             Knowledge of TailwindCSS, Material UI and most popular technologies.
           </p>
         </article>
-        <article className="p-12 border-2 rounded-2xl max-w-screen-sm">
-          <h3 className="text-xl md:text-2xl font-medium">
+        <article className="p-6 md:p-12 border-2 rounded-2xl max-w-screen-sm md:w-[500px]">
+          <h3 className="text-lg md:text-xl lg:text-2xl font-medium">
             Front-End Developer
           </h3>
-          <p className="text-sm md:text-xl font-normal">
+          <p className="text-xs md:text-sm lg:text-xl font-normal">
             Experienced in JavaScript, HTML, CSS and React framework.
           </p>
-          <p className="text-sm md:text-xl font-normal">
+          <p className="text-xs md:text-sm lg:text-xl font-normal">
             Knowledge of TailwindCSS, Material UI and most popular technologies.
           </p>
         </article>
